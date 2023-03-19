8bdebb94945d371f066d937d1d527f300c99aff1
RazielHernandez <razo.puma.azteca@gmail.com>
2023-03-19 12:39:25 -0400

    First commit. Project downloaded. Fixed some minors update bugs and geolocator added

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/razielhernandez/Documents/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/razielhernandez/Documents/Flutter/Clima-Flutter-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/razielhernandez/Documents/Flutter/Clima-Flutter-master/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/razielhernandez/Documents/Flutter/Clima-Flutter-master/.dart_tool/package_config.json"
