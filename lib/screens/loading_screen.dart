29262098ccdd0c5f61e310eaf40e6cfc765170f8
RazielHernandez <razo.puma.azteca@gmail.com>
2023-03-20 14:27:54 -0400

    Second commit. HTTP getData added with static Uri

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,19 +9,28 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  void getLocatoin() async {
-
-    Location location = Location();
-    await location.getCurrentPosition();
-    print("latitude: ${location.latitude}");
-    print("longitud: ${location.longitud}");
-  }
-
   @override
   void initState() {
     getLocatoin();
     super.initState();
   }
+  void getLocatoin() async {
+    Location location = Location();
+    await location.getCurrentPosition();
+    getData();
+    print("latitude: ${location.latitude}");
+    print("longitud: ${location.longitud}");
+  }
+
+  void getData() async {
+    http.Response response = await http.get(Uri.parse("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d94f22decde6dd8ec8a8f44dcfa74636"));
+    if (response.statusCode == 200){
+      print(response.body);
+    } else {
+      print("API error with code: ${response.statusCode}");
+    }
+  }
+  
 
   @override
   Widget build(BuildContext context) {
