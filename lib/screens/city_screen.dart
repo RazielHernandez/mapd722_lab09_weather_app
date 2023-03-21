d166a4ddd60a1116aa4de7a58925951ed5fe8d7e
RazielHernandez <razo.puma.azteca@gmail.com>
2023-03-20 23:21:33 -0400

    Final commit. City screen (custom city) added

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -7,6 +7,8 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+
+  String cityName = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,40 +27,36 @@ class _CityScreenState extends State<CityScreen> {
                 alignment: Alignment.topLeft,
                 child: 
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                     child: Icon(
                       Icons.arrow_back_ios,
                       size: 50.0,
                     ),
                   )
-                  /*
-                  FlatButton(
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.arrow_back_ios,
-                      size: 50.0,
-                    ),
-                  ),*/
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black
+                  ),
+                  decoration: KInputDecoration,
+                  onChanged: (value) {
+                    cityName = value;
+                  },
+                ),
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: Text(
                   'Get Weather',
                   style: kButtonTextStyle,
                 ),
               ),
-              /*
-              FlatButton(
-                onPressed: () {},
-                child: Text(
-                  'Get Weather',
-                  style: kButtonTextStyle,
-                ),
-              ),*/
             ],
           ),
         ),
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -2,19 +2,33 @@ import 'package:flutter/material.dart';
 
 const kTempTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 100.0,
+  fontSize: 80.0,
 );
 
 const kMessageTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 60.0,
+  fontSize: 45.0,
 );
 
 const kButtonTextStyle = TextStyle(
-  fontSize: 30.0,
+  fontSize: 23.0,
   fontFamily: 'Spartan MB',
 );
 
 const kConditionTextStyle = TextStyle(
-  fontSize: 100.0,
+  fontSize: 80.0,
+);
+
+const KInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(Icons.location_city, color: Colors.white),
+  hintText: "Enter the city name",
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder (
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderSide: BorderSide.none
+  )
 );
